99e365ab471bcd7b24b5d7fd1d54290b47fdd91f
Surya <suryapanneer04@gmail.com>
2024-04-16 21:59:47 +0530

    Arithmatic operator

---
+++ Example6.js
@@ -0,0 +1,40 @@
+//Arithmatic operators 
+
+let x=10,y=10,z;
+z=x+y;
+console.log(z);
+z=x-y;
+console.log(z);
+z=x*y;
+console.log(z);
+z=x/y;
+console.log(z);
+z=x%y;
+console.log(z);
+
+
+function arith(){
+    let x=10,y=10,z;
+ z=x+y;
+console.log("Addition value is :"+z);
+z=x-y;
+console.log("Subtraction value is :"+z);
+z=x*y;
+console.log("Multiplication value is :"+z);
+z=x/y;
+console.log("Division value is :"+z);
+z=x%y;
+console.log("Modules value is :"+z); 
+
+}
+arith();
+
+// function comparision(){
+//     let a=10,b=20,z;
+//     z=a>b;
+//     console.log(z);
+//     z=a<b;
+//     console.log(z);
+
+// }
+// comparision();
